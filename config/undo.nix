641b0a6c948dcd0ce0f7f93249573b3863502706
Elias Eskelinen <elias.eskelinen@pm.me>
2024-08-05 14:08:35 +0300

    add undotree

---
+++ config/undo.nix
@@ -0,0 +1,10 @@
+{
+  plugins.undotree.enable = true;
+  keymaps = [
+    {
+      key = "<leader><F5>";
+      action = "<cmd>UndotreeToggle<cr>";
+      options = { desc = "Toggle Undo Tree"; };
+    }
+  ];
+}
