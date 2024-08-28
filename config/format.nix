10c985c77dd8b17f412eae20a9f51a7cea56f88b
Elias Eskelinen <git@eliaseskelinen.fi>
2024-08-28 20:57:22 +0300

    update nixvim

--- config/format.nix
+++ config/format.nix
@@ -3,24 +3,26 @@
 {
   plugins.conform-nvim = {
     enable = true;
-    formatOnSave = {
-      lspFallback = true;
-      timeoutMs = 500;
-    };
-    notifyOnError = true;
-    formattersByFt = {
-      html = [ [ "prettierd" "prettier" ] ];
-      css = [ [ "prettierd" "prettier" ] ];
-      javascript = [ [ "prettierd" "prettier" ] ];
-      javascriptreact = [ [ "prettierd" "prettier" ] ];
-      typescript = [ [ "prettierd" "prettier" ] ];
-      typescriptreact = [ [ "prettierd" "prettier" ] ];
-      python = [ "black" ];
-      lua = [ "stylua" ];
-      nix = [ "nixpkgs-fmt" ];
-      rust = [ "rustfmt" ];
-      markdown = [ [ "prettierd" "prettier" ] ];
-      yaml = [ "yamllint" "yamlfmt" ];
+    settings = {
+      format_on_save = {
+        lspFallback = true;
+        timeoutMs = 500;
+      };
+      notifyOnError = true;
+      formattersByFt = {
+        html = [ [ "prettierd" "prettier" ] ];
+        css = [ [ "prettierd" "prettier" ] ];
+        javascript = [ [ "prettierd" "prettier" ] ];
+        javascriptreact = [ [ "prettierd" "prettier" ] ];
+        typescript = [ [ "prettierd" "prettier" ] ];
+        typescriptreact = [ [ "prettierd" "prettier" ] ];
+        python = [ "black" ];
+        lua = [ "stylua" ];
+        nix = [ "nixpkgs-fmt" ];
+        rust = [ "rustfmt" ];
+        markdown = [ [ "prettierd" "prettier" ] ];
+        yaml = [ "yamllint" "yamlfmt" ];
+      };
     };
   };
   extraPackages = with pkgs; [ black pylint prettierd stylua nixpkgs-fmt yamllint yamlfmt rustfmt ];
