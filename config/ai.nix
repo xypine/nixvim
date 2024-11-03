883c87c8ff466a5ecf98641189eb10bd83c229b8
Elias Eskelinen <git@eliaseskelinen.fi>
2024-11-03 23:39:03 +0200

    fix: disable ai for now

--- config/ai.nix
+++ config/ai.nix
@@ -1,13 +1,39 @@
+{ pkgs, ... }:
+
 {
-  plugins.copilot-lua = {
-    enable = true;
-    suggestion = { enabled = false; };
-    panel = { enabled = false; };
-  };
-  plugins.copilot-chat = {
-    enable = true;
-  };
-  plugins.copilot-cmp = {
-    enable = true;
-  };
+  # extraPlugins = with pkgs.vimPlugins; [
+  #   avante-nvim
+  # ];
+  # extraConfigLuaPre = ''
+  #   require('avante_lib').load()
+  #   require('avante').setup {
+  #     provider = "remote-ollama",
+  #     auto_suggestions_provider = "remote-ollama",
+  #     vendors = {
+  #       ["remote-ollama"] = {
+  #         ["local"] = true,
+  #         endpoint = "eepc:11434/v1",
+  #         model = "llama3.2",
+  #         parse_curl_args = function(opts, code_opts)
+  #            return {
+  #              url = opts.endpoint .. "/chat/completions",
+  #              headers = {
+  #                ["Accept"] = "application/json",
+  #                ["Content-Type"] = "application/json",
+  #              },
+  #              body = {
+  #                model = opts.model,
+  #                messages = require("avante.providers").copilot.parse_message(code_opts), -- you can make your own message, but this is very advanced
+  #                max_tokens = 2048,
+  #                stream = true,
+  #              },
+  #            }
+  #          end,
+  #          parse_response_data = function(data_stream, event_state, opts)
+  #            require("avante.providers").openai.parse_response(data_stream, event_state, opts)
+  #          end,
+  #       }
+  #     },
+  #   }
+  # '';
 }
