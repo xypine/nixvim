dd7f30b85356ca9f2cc5e794543f98717fa4f7d5
Elias Eskelinen <git@eliaseskelinen.fi>
2025-10-05 18:44:16 +0300

    feat: Add git hunk navigation

--- config/git.nix
+++ config/git.nix
@@ -1,11 +1,33 @@
 {
-  plugins.gitsigns = {
-    enable = true;
+  plugins = {
+    gitsigns = {
+      enable = true;
+    };
+    gitblame = {
+      enable = true;
+    };
+    fugitive = {
+      enable = true;
+    };
   };
-  plugins.gitblame = {
-    enable = true;
-  };
-  plugins.fugitive = {
-    enable = true;
-  };
+
+  # Git hunk navigation
+  keymaps = [
+    {
+      mode = "n";
+      key = "]c";
+      action = "<cmd>Gitsigns next_hunk<cr>";
+      options = {
+        desc = "Next git hunk";
+      };
+    }
+    {
+      mode = "n";
+      key = "[c";
+      action = "<cmd>Gitsigns prev_hunk<cr>";
+      options = {
+        desc = "Previous git hunk";
+      };
+    }
+  ];
 }
