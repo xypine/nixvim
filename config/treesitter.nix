8c5fb0e9a631f6b1d946e7f92d8baa94c4e4600a
Elias Eskelinen <git@eliaseskelinen.fi>
2025-12-27 15:38:14 +0200

    fix: treesitter regression

--- config/treesitter.nix
+++ config/treesitter.nix
@@ -6,8 +6,15 @@
 {
   plugins.treesitter = {
     enable = true;
+
     highlight.enable = true;
     indent.enable = true;
+
+    #  TODO: remove once once the above work
+    settings = {
+      highlight.enable = true;
+      indent.enable = true;
+    };
   };
 
   plugins.ts-autotag = {
